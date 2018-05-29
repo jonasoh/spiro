ae5e6f0b6e213396e4048b5a43227be8860f318c
Jonas Ohlsson <jonas@vansinne.se>
2018-05-29 16:47:27 +0200

    More code style fixes.

--- plantlapse.py
+++ plantlapse.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 #
-# plantlapse.py - 
-#   script for time-lapse imaging of petri dishes, with a focus on plants (i.e. it is adapted to day/night cycles). 
-#   designed to be used with the 5 MP OV5647-based camera with IR illumation, which is widely available. 
-# 
+# plantlapse.py -
+#   script for time-lapse imaging of petri dishes, with a focus on plants (i.e. it is adapted to day/night cycles).
+#   designed to be used with the 5 MP OV5647-based camera with IR illumation, which is widely available.
+#
 # - Jonas Ohlsson <jonas.ohlsson .a. slu.se>
 #
 
@@ -17,32 +17,33 @@ version = "0.1.0"
 
 parser = OptionParser(version="plantlapse " + version)
 parser.add_option("-n", "--num-shots", default=168, type="int", dest="nshots",
-                    help="number of shots to capture [default: 168]")
-parser.add_option("-d", "--delay", type="float", default=60, dest="delay", 
-                    help="time, in minutes, to wait between shots [default: 60]")
-parser.add_option("--day-shutter", default=50, dest="dayshutter", type="int", 
-                    help="daytime shutter in fractions of a second, i.e. for 1/100 specify '100' [default: 50]")
-parser.add_option("--night-shutter", default=200, dest="nightshutter", type="int", 
-                    help="nighttime shutter in fractions of a second [default: 200]")
+                  help="number of shots to capture [default: 168]")
+parser.add_option("-d", "--delay", type="float", default=60, dest="delay",
+                  help="time, in minutes, to wait between shots [default: 60]")
+parser.add_option("--day-shutter", default=50, dest="dayshutter", type="int",
+                  help="daytime shutter in fractions of a second, i.e. for 1/100 specify '100' [default: 50]")
+parser.add_option("--night-shutter", default=200, dest="nightshutter", type="int",
+                  help="nighttime shutter in fractions of a second [default: 200]")
 parser.add_option("--day-iso", default=100, dest="dayiso", type="int",
-                    help="set daytime ISO value (0=auto) [default: 100]")
+                  help="set daytime ISO value (0=auto) [default: 100]")
 parser.add_option("--night-iso", default=100, dest="nightiso", type="int",
-                    help="set nighttime ISO value (0=auto) [default: 100]")
-parser.add_option("--resolution", default="2592x1944", dest="resolution", 
-                    help="set camera resolution [default: 2592x1944]")
-parser.add_option("--dir", default=".", dest="dir", 
-                    help="output pictures to directory 'DIR', creating it if needed [default: use current directory]")
-parser.add_option("--prefix", default="", dest="prefix", 
-                    help="prefix to use for filenames [default: none]")
+                  help="set nighttime ISO value (0=auto) [default: 100]")
+parser.add_option("--resolution", default="2592x1944", dest="resolution",
+                  help="set camera resolution [default: 2592x1944]")
+parser.add_option("--dir", default=".", dest="dir",
+                  help="output pictures to directory 'DIR', creating it if needed [default: use current directory]")
+parser.add_option("--prefix", default="", dest="prefix",
+                  help="prefix to use for filenames [default: none]")
 parser.add_option("--auto-wb", default=False, action="store_true", dest="awb",
-                    help="adjust white balance between shots (if false, only adjust when day/night shift is detected) [default: false]")
+                  help="adjust white balance between shots (if false, only adjust when day/night shift is detected) [default: false]")
 parser.add_option("--led", default=False, action="store_true", dest="led",
-                    help="do not disable camera led; useful for running without GPIO privileges")
+                  help="do not disable camera led; useful for running without GPIO privileges")
 parser.add_option("--preview", action="store_true", default=False, dest="preview",
-                    help="show a live preview of the current settings for 60 seconds, then exit")
-parser.add_option("-t","--test", action="store_true", default=False, dest="test", 
-                    help="capture a test picture as 'test.jpg', then exit")
+                  help="show a live preview of the current settings for 60 seconds, then exit")
+parser.add_option("-t", "--test", action="store_true", default=False, dest="test",
+                  help="capture a test picture as 'test.jpg', then exit")
 (options, args) = parser.parse_args()
+
 
 def initCam():
     cam = PiCamera()
@@ -51,6 +52,7 @@ def initCam():
     if not options.led: 
         cam.led = False
     return cam
+
 
 def isDaytime():
     # determine if it's day or not. give the camera 1 second to adjust.
@@ -61,12 +63,13 @@ def isDaytime():
     print("Exposure speed: %i" % exp)
     return exp < 20000
 
+
 def setWB():
     sys.stdout.write("Determining white balance")
     cam.awb_mode = "auto"
     (one, two) = cam.awb_gains
     sys.stdout.flush()
-    for i in range(1,5):
+    for i in range(4):
         time.sleep(1)
         sys.stdout.write(".")
         sys.stdout.flush()
@@ -95,7 +98,7 @@ if options.dir != ".":
 for n in range(options.nshots):
     prev_daytime = daytime
     daytime = isDaytime()
-    
+
     # set new wb if there's a day/night shift
     if prev_daytime != daytime and not options.awb:
         setWB()
@@ -106,12 +109,12 @@ for n in range(options.nshots):
     else:
         cam.shutter_speed = 1000000//options.nightshutter
         cam.iso=options.nightiso
-    
+
     if options.test:
         cam.capture(os.path.join(options.dir, "test.jpg"))
         print("Test picture captured successfully.")
         sys.exit()
-    
+
     now = time.strftime("%Y%m%d-%H%M%S", time.localtime())
     filename = os.path.join(options.dir, options.prefix + now + ".jpg")
     sys.stdout.write("Capturing %s..." % filename)
