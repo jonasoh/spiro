c0fedee3780b894c8425c080baa4cfc79d8de156
Jonas Ohlsson <jonas@vansinne.se>
2019-05-22 16:18:24 +0200

    make python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(name = 'spiro',
+      version = '1.0a1',
+      package_dir = {'': 'src'},
+      packages = find_packages(),
+      scripts = ['bin/spiro'],
+      install_requires = ['picamera==1.13', 'RPi.GPIO==0.6.5'],
+      author = 'Jonas Ohlsson',
+      author_email = 'jonas.ohlsson@slu.se',
+      description = 'Control software for the SPIRO biological imaging system',
+      url = 'https://github.com/jonasoh/spiro',
+      zip_safe = True,
+      )
