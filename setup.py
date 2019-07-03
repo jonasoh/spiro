2700a1043906d68f90a6766123cae6851c0ece05
Jonas Ohlsson <jonas@vansinne.se>
2019-07-03 11:58:31 +0200

    fix directory confusion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(name = 'spiro',
       version = '1.0a1',
       package_dir = {'': 'src'},
-      packages = find_packages(),
+      packages = find_packages(where='src'),
       scripts = ['bin/spiro'],
       install_requires = ['picamera==1.13', 'RPi.GPIO==0.6.5'],
       author = 'Jonas Ohlsson',
