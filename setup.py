0f3670a449697b2e6d01667d2d19fe4828ef840c
Jonas Ohlsson <jonas@vansinne.se>
2019-07-23 17:06:48 +0200

    make sure data files are installed with package. bump to 1.0b1.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name = 'spiro',
-      version = '1.0a1',
+      version = '1.0b1',
       packages = find_packages(),
       scripts = ['bin/spiro'],
       install_requires = ['picamera==1.13', 'RPi.GPIO==0.6.5', 'Flask==1.1.1', 'waitress==1.3.0'],
@@ -8,5 +8,5 @@ setup(name = 'spiro',
       author_email = 'jonas.ohlsson@slu.se',
       description = 'Control software for the SPIRO biological imaging system',
       url = 'https://github.com/jonasoh/spiro',
-      zip_safe = True,
+      zip_safe = False,
       )
