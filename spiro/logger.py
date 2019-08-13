57b18926b27021a666600e1fb03c45b30fcf042c
Jonas Ohlsson <jonas@vansinne.se>
2019-08-13 09:42:31 +0200

    add debug logger.

--- spiro/logger.py
+++ spiro/logger.py
@@ -1,5 +1,13 @@
 import sys
+from spiro.config import Config
+
+cfg = Config()
 
 def log(msg):
     sys.stderr.write(msg + '\n')
     sys.stderr.flush()
+
+def debug(msg):
+    if cfg.get('debug'):
+        sys.stderr.write(msg + '\n')
+        sys.stderr.flush()
